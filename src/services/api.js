500ccfeb46b82b5c8b41ec67e7780d2401d5b399
agar-ikshit <121373055+agar-ikshit@users.noreply.github.com>
2025-07-04 10:48:46 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://torus-backend-xr0w.onrender.com/api' });
 
 export const loginUser = (credentials) => API.post('/auth/login', credentials);
 export const registerUser = (userData) => API.post('/auth/register', userData);
